2548a8046a0a6f33ced71dcec409dcc1cd3c439d
Fallest <56403805+Fallest@users.noreply.github.com>
2021-05-04 07:24:59 +0200

    Tarea PRO
    Terminada la primera tarea de herencia (UD07_2_1, tarea de personas).

--- Bloque 2 - Java/Ejercicios/TareaUD07_2/src/main/java/Ej_HerenciaPersonas/main.java
+++ Bloque 2 - Java/Ejercicios/TareaUD07_2/src/main/java/Ej_HerenciaPersonas/main.java
@@ -8,17 +8,34 @@ public class main {
     public static void main(String[] args) {
         // Variables
         int cantMaquinistas, cantAdministrativos, cantPersonas;
+        Persona p[];
+        Maquinista m[];
+        Administrativo a[];
         
         // Preguntar al usuario cuántas personas hay de cada tipo
         cantMaquinistas = pedirCantMaquinistas();
         cantAdministrativos = peditCantAdministrativos();
         cantPersonas = pedirCantPersonas();
         
+        p = new Persona[cantPersonas];
+        m = new Maquinista[cantMaquinistas];
+        a = new Administrativo[cantAdministrativos];
         
+        introducirDatos(p);
+        introducirDatos(m);
+        introducirDatos(a);
+        
+        mostrarDatos(p);
+        mostrarDatos(m);
+        mostrarDatos(a);
+        
+        calcMedia(p);
+        calcMedia(m);
+        calcMedia(a);
     }
 
     private static int pedirCantMaquinistas() {
-        int cantMaquinistas;
+        int cantMaquinistas = 0;
         
         boolean ejec;
         InputStreamReader stream = new InputStreamReader(System.in);
@@ -27,7 +44,7 @@ public class main {
         do {
             try {
                 ejec = false;
-                System.out.println("\nIntroduzca el nivel de estudio del administrador \n(ESO, Bachillerato, Grado universitario...): ");
+                System.out.println("\nIntroduzca la cantidad de maquinistas: ");
                 cantMaquinistas = Integer.parseInt(teclado.readLine());
             }
             catch (IOException e) {
@@ -44,15 +61,107 @@ public class main {
     }
 
     private static int peditCantAdministrativos() {
-        int cantAdministrativos;
+        int cantAdministrativos = 0;
         
+        boolean ejec;
+        InputStreamReader stream = new InputStreamReader(System.in);
+        BufferedReader teclado = new BufferedReader(stream);
         
-        return cantAdministrativos,
+        do {
+            try {
+                ejec = false;
+                System.out.println("\nIntroduzca la cantidad de administrativos: ");
+                cantAdministrativos = Integer.parseInt(teclado.readLine());
+            }
+            catch (IOException e) {
+                System.out.println("\n\tError en la entrada de datos\n");
+                ejec = true;
+            }
+            catch (NumberFormatException e) {
+                System.out.println("\n\tError: Introduzca un número válido.\n");
+                ejec = true;
+            }
+        } while (ejec);
+        
+        return cantAdministrativos;
     }
 
     private static int pedirCantPersonas() {
-        int cantPersonas;
+        int cantPersonas = 0;
+        
+        boolean ejec;
+        InputStreamReader stream = new InputStreamReader(System.in);
+        BufferedReader teclado = new BufferedReader(stream);
+        
+        do {
+            try {
+                ejec = false;
+                System.out.println("\nIntroduzca la cantidad de personas que no son \nmaquinistas ni administrativos: : ");
+                cantPersonas = Integer.parseInt(teclado.readLine());
+            }
+            catch (IOException e) {
+                System.out.println("\n\tError en la entrada de datos\n");
+                ejec = true;
+            }
+            catch (NumberFormatException e) {
+                System.out.println("\n\tError: Introduzca un número válido.\n");
+                ejec = true;
+            }
+        } while (ejec);
         
         return cantPersonas;
+    }
+    
+    private static void introducirDatos(Persona p[]) {
+        int i;
+        
+        if (p[0] instanceof Persona)
+            for (i = 0; i < p.length; i++)
+                p[i] = new Persona();
+        else if (p[0] instanceof Maquinista)
+            for (i = 0; i < p.length; i++)
+                p[i] = new Maquinista();
+        else if (p[0] instanceof Administrativo)
+            for (i = 0; i < p.length; i++)
+                p[i] = new Administrativo();
+    }
+
+    private static void mostrarDatos(Persona p[]) {
+        int i;
+        
+        for (i = 0; i < p.length; i++)
+            p[i].mostrarDatos();
+    }
+    
+    private static void calcMedia(Persona p[]) {
+        int i, suma = 0, cont = 0;
+        float media;
+        
+        if (p[0] instanceof Persona) {
+            for (i = 0; i < p.length; i++) {
+                suma += p[i].getEdad();
+                cont ++;
+            }
+            media = (float) suma / cont;
+            System.out.println("Media de edad de personas: " + media);
+        }
+        
+        else if (p[0] instanceof Maquinista) {
+            for (i = 0; i < p.length; i++) {
+                suma += p[i].getEdad();
+                cont ++;
+            }
+            media = (float) suma / cont;
+            System.out.println("Media de edad de maquinistas: " + media);
+        }
+        
+        else if (p[0] instanceof Administrativo) {
+            for (i = 0; i < p.length; i++) {
+                suma += p[i].getEdad();
+                cont ++;
+            }
+            media = (float) suma / cont;
+            System.out.println("Media de edad de administrativos: " + media);
+        }
     }
 }
