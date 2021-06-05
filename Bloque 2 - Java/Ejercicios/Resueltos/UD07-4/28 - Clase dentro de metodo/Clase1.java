64440dcc653105c7333202fce5ae3de26cb21042
Fallest <56403805+Fallest@users.noreply.github.com>
2021-06-05 11:49:43 +0200

    Update
    Actualizados algunos archivos.
    Añadido un archivo con un enlace a los vídeos de los proyectos de Java.

--- Bloque 2 - Java/Ejercicios/Resueltos/UD07-4/28 - Clase dentro de metodo/Clase1.java
+++
@@ -1,8 +0,0 @@
-public class Clase1
-{
-  public static void main(String[] args)
-  {
-    Clase2 obj = new Clase2();
-	obj.metClase2(1, 2);
-  }
-}
--- Bloque 2 - Java/Ejercicios/Resueltos/UD07-4/29 - Clase anonima/Test2.java
+++
@@ -1,9 +0,0 @@
-public class Test
-{
-  public static void main(String args[])
-  {
-    CPersona2 unaPersona = new CPersona2("Un nombre", 22, 2, 2014);
-	System.out.println(unaPersona.obtenerNombre());
-	System.out.println(unaPersona.obtenerFechaNa());
-  }
-}
